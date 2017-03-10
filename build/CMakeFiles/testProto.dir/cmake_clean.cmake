4aecb298976b23664f8b02b8293f962b887ae753
cvrsg <jianping2014@126.com>
2017-03-10 17:14:12 +0800

    An example of protobuf

---
+++ build/CMakeFiles/testProto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testProto.dir/parse.cpp.o"
+  "CMakeFiles/testProto.dir/test.pb.cc.o"
+  "testProto.pdb"
+  "testProto"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testProto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
